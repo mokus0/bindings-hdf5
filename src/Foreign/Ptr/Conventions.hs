67d8d8d55e0f5f2f2954f8f036937007999232aa
James Cook <mokus@deepbondi.net>
2011-05-04 14:28:51 -0400

    Reorganized Foreign.Ptr.Conventions.  WARNING: changed order of returns from withOut*.

--- src/Foreign/Ptr/Conventions.hs
+++ src/Foreign/Ptr/Conventions.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+-- |various common data-passing conventions
 module Foreign.Ptr.Conventions where
 
 -- TODO: make these all exception-safe
@@ -15,31 +16,33 @@ import Control.Monad.Primitive (RealWorld)
 
 import qualified Data.Vector.Storable as SV
 
--- various common data-passing conventions
+class WrappedPtr p where
+    wrapPtr         :: Ptr a -> p a
+    unwrapPtr       :: p a -> Ptr a
+    nullWrappedPtr  :: p a
+    nullWrappedPtr = wrapPtr nullPtr
+    castWrappedPtr  :: p a -> p b
+    castWrappedPtr = wrapPtr . castPtr . unwrapPtr
+
+instance WrappedPtr Foreign.Ptr.Ptr where
+    wrapPtr         = id
+    unwrapPtr       = id
+    nullWrappedPtr  = nullPtr
+    castWrappedPtr  = castPtr
+
+-- * Input pointers
 
 -- |In by-ref parameter; memory is allocated and freed by caller
-newtype In       a = In       (Ptr a) deriving (Eq, Ord, Show, Storable)
+newtype In       a = In       (Ptr a) deriving (Eq, Ord, Show, Storable, WrappedPtr)
 
 -- |In by-ref array; memory is allocated and freed by caller
-newtype InArray  a = InArray  (Ptr a) deriving (Eq, Ord, Show, Storable)
-
--- |Out by-ref parameter; memory is allocated and freed by caller
-newtype Out      a = Out      (Ptr a) deriving (Eq, Ord, Show, Storable)
-
--- |Out by-ref array; length is specified by caller, memory is allocated
--- and freed by caller
-newtype OutArray a = OutArray (Ptr a) deriving (Eq, Ord, Show, Storable)
-
--- |In-out parameter.  Memory is allocated and freed by caller.
-newtype InOut    a = InOut    (Ptr a) deriving (Eq, Ord, Show, Storable)
-
-newtype InOutArray a = InOutArray (Ptr a) deriving (Eq, Ord, Show, Storable)
+newtype InArray  a = InArray  (Ptr a) deriving (Eq, Ord, Show, Storable, WrappedPtr)
 
 withIn :: (Storable a, MonadPeelIO m) => a -> (In a -> m b) -> m b
 withIn x f = liftIOOp (with x) (f . In)
 
-withInArray :: (Storable a, MonadPeelIO m) => [a] -> (InArray a -> m b) -> m b
-withInArray xs f = liftIOOp (withArray xs) (f . InArray)
+withInList :: (Storable a, MonadPeelIO m) => [a] -> (InArray a -> m b) -> m b
+withInList xs f = liftIOOp (withArray xs) (f . InArray)
 
 withInVector :: (Storable a, MonadPeelIO m) => SV.Vector a -> (InArray a -> m b) -> m b
 withInVector vec f = liftIOOp (SV.unsafeWith vec) (f . InArray)
@@ -47,11 +50,20 @@ withInVector vec f = liftIOOp (SV.unsafeWith vec) (f . InArray)
 withInMVector :: (Storable a, MonadPeelIO m) => SV.MVector RealWorld a -> (InArray a -> m b) -> m b
 withInMVector (SV.MVector p _ fp) f = liftIOOp (withForeignPtr fp) (\_ -> f (InArray p))
 
-withOut :: (Storable a, MonadPeelIO m) => (Out a -> m b) -> m (b,a)
+-- * Output pointers
+
+-- |Out by-ref parameter; memory is allocated and freed by caller
+newtype Out      a = Out      (Ptr a) deriving (Eq, Ord, Show, Storable, WrappedPtr)
+
+-- |Out by-ref array; length is specified by caller, memory is allocated
+-- and freed by caller
+newtype OutArray a = OutArray (Ptr a) deriving (Eq, Ord, Show, Storable, WrappedPtr)
+
+withOut :: (Storable a, MonadPeelIO m) => (Out a -> m b) -> m (a,b)
 withOut f = liftIOOp alloca $ \p -> do
     b <- f (Out p)
     a <- liftIO (peek p)
-    return (b,a)
+    return (a,b)
 
 withMaybeOut :: (Storable a, MonadPeelIO m) => (Out a -> m Bool) -> m (Maybe a)
 withMaybeOut f = liftIOOp alloca $ \p -> do
@@ -67,45 +79,15 @@ withOut_ f = liftIOOp alloca $ \p -> do
     f (Out p)
     liftIO (peek p)
 
-withOutArray :: (Storable a, MonadIO m) => Int -> (OutArray a -> m b) -> m (b,[a])
-withOutArray n f = do
-    p <- liftIO (mallocArray n)
-    b <- f (OutArray p)
-    a <- liftIO (peekArray n p)
-    liftIO (free p)
-    return (b,a)
-
 withOutMVector :: (Storable a, MonadPeelIO m) => SV.MVector RealWorld a -> (Int -> OutArray a -> m b) -> m b
 withOutMVector (SV.MVector p n fp) f = do
     liftIOOp (withForeignPtr fp) (\_ -> f n (OutArray p))
 
-withOutVector :: (Storable a, MonadPeelIO m) => Int -> (OutArray a -> m b) -> m (b, SV.Vector a)
+withOutVector :: (Storable a, MonadPeelIO m) => Int -> (OutArray a -> m b) -> m (SV.Vector a, b)
 withOutVector n f = do
     p <- liftIO (mallocForeignPtrArray n)
     b <- liftIOOp (withForeignPtr p) (f . OutArray)
-    return (b, SV.unsafeFromForeignPtr p 0 n)
-
-withOutArray' :: (Storable a, MonadIO m) => Int -> (OutArray a -> m Int) -> m (Int, [a])
-withOutArray' sz f = do
-    p <- liftIO (mallocArray sz)
-    n <- f (OutArray p)
-    a <- liftIO (peekArray n p)
-    liftIO (free p)
-    return (n, a)
-
-withOutVector' :: (Storable a, MonadPeelIO m) => Int -> (OutArray a -> m Int) -> m (SV.Vector a)
-withOutVector' sz f = do
-    p <- liftIO (mallocForeignPtrArray sz)
-    n <- liftIOOp (withForeignPtr p) (f . OutArray)
-    return (SV.unsafeFromForeignPtr p 0 n)
-
-withOutArray_ :: (Storable a, MonadIO m) => Int -> (OutArray a -> m b) -> m [a]
-withOutArray_ n f = do
-    p <- liftIO (mallocArray n)
-    f (OutArray p)
-    a <- liftIO (peekArray n p)
-    liftIO (free p)
-    return a
+    return (SV.unsafeFromForeignPtr p 0 n, b)
 
 withOutVector_ :: (Storable a, MonadPeelIO m) => Int -> (OutArray a -> m b) -> m (SV.Vector a)
 withOutVector_ n f = do
@@ -113,25 +95,61 @@ withOutVector_ n f = do
     b <- liftIOOp (withForeignPtr p) (f . OutArray)
     return (SV.unsafeFromForeignPtr p 0 n)
 
+withOutVector' :: (Storable a, MonadPeelIO m) => Int -> (OutArray a -> m Int) -> m (SV.Vector a)
+withOutVector' sz f = do
+    p <- liftIO (mallocForeignPtrArray sz)
+    n <- liftIOOp (withForeignPtr p) (f . OutArray)
+    return (SV.unsafeFromForeignPtr p 0 n)
 
--- | @withOut0 zero n f@: allocate an array large enough to hold @n@ elements,
+withOutList :: (Storable a, MonadIO m) => Int -> (OutArray a -> m b) -> m ([a],b)
+withOutList n f = do
+    p <- liftIO (mallocArray n)
+    b <- f (OutArray p)
+    a <- liftIO (peekArray n p)
+    liftIO (free p)
+    return (a, b)
+
+withOutList_ :: (Storable a, MonadIO m) => Int -> (OutArray a -> m b) -> m [a]
+withOutList_ n f = do
+    p <- liftIO (mallocArray n)
+    f (OutArray p)
+    a <- liftIO (peekArray n p)
+    liftIO (free p)
+    return a
+
+withOutList' :: (Storable a, MonadIO m) => Int -> (OutArray a -> m Int) -> m ([a], Int)
+withOutList' sz f = do
+    p <- liftIO (mallocArray sz)
+    n <- f (OutArray p)
+    a <- liftIO (peekArray n p)
+    liftIO (free p)
+    return (a, n)
+
+-- | @withOutList0 zero n f@: allocate an array large enough to hold @n@ elements,
 -- plus one extra spot for a terminator.  Calls @f@ with that buffer, which is
 -- expected to fill it with up to @n@ elements, followed by @zero@.  The 
 -- elements are then read out into a list.
-withOut0 :: (Storable a, Eq a, MonadIO m) => a -> Int -> (OutArray a -> m b) -> m (b,[a])
-withOut0 zero n f = do
+withOutList0 :: (Storable a, Eq a, MonadIO m) => a -> Int -> (OutArray a -> m b) -> m ([a], b)
+withOutList0 zero n f = do
     p <- liftIO (mallocArray0 n)
     b <- f (OutArray p)
     a <- liftIO (peekArray0 zero p)
     liftIO (free p)
-    return (b,a)
+    return (a, b)
 
-withInOut :: (Storable a, MonadPeelIO m) => a -> (InOut a -> m b) -> m (b,a)
+-- * Bidirectional pointers
+
+-- |In-out parameter.  Memory is allocated and freed by caller.
+newtype InOut    a = InOut    (Ptr a) deriving (Eq, Ord, Show, Storable, WrappedPtr)
+
+newtype InOutArray a = InOutArray (Ptr a) deriving (Eq, Ord, Show, Storable, WrappedPtr)
+
+withInOut :: (Storable a, MonadPeelIO m) => a -> (InOut a -> m b) -> m (a,b)
 withInOut a f = liftIOOp alloca $ \p -> do
     liftIO (poke p a)
     b <- f (InOut p)
     a <- liftIO (peek p)
-    return (b,a)
+    return (a,b)
 
 withInOut_ :: (Storable a, MonadPeelIO m) => a -> (InOut a -> m b) -> m a
 withInOut_ a f = liftIOOp alloca $ \p -> do
@@ -139,8 +157,8 @@ withInOut_ a f = liftIOOp alloca $ \p -> do
     f (InOut p)
     liftIO (peek p)
 
-withInOutArray :: (Storable a, MonadIO m) => Int -> [a] -> (InOutArray a -> m (Int, b)) -> m ([a], b)
-withInOutArray sz xs f = do
+withInOutList :: (Storable a, MonadIO m) => Int -> [a] -> (InOutArray a -> m (Int, b)) -> m ([a], b)
+withInOutList sz xs f = do
     p <- liftIO (mallocArray sz)
     liftIO $ sequence_
         [ pokeElemOff p i x
@@ -156,8 +174,8 @@ withInOutArray sz xs f = do
     
     return (xs', y)
 
-withInOutArray_ :: (Storable a, MonadIO m) => Int -> [a] -> (InOutArray a -> m Int) -> m [a]
-withInOutArray_ sz xs f = do
+withInOutList_ :: (Storable a, MonadIO m) => Int -> [a] -> (InOutArray a -> m Int) -> m [a]
+withInOutList_ sz xs f = do
     p <- liftIO (mallocArray sz)
     liftIO $ sequence_
         [ pokeElemOff p i x
